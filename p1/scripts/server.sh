f345a6d50d33d785b0f46f4a883a5f7b09113841
mikelezc <138023008+mikelezc@users.noreply.github.com>
2026-08-04 16:43:10 +0200

    refactor p1

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-# scripts/server.sh
 
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 # Modo estricto: detiene el script ante cualquier error o variable no definida.
 set -euo pipefail
 
-# El script recibe la IP del servidor como argumento. (Vagrantfile)
+# El script recibe la IP del servidor como argumento desde el Vagrantfile.
 SERVER_IP=$1
-
-# Obtenemos la interfaz de red que tiene asignada la IP del servidor. (Desde la VM)
+# Calculamos la interfaz de red que tiene asignada la IP del nodo worker. (Desde la VM)
 IFACE=$(ip -4 addr show | grep $SERVER_IP | awk '{print $NF}')
 
 
@@ -18,9 +16,9 @@ echo -e "${CYAN}=========================================================${NC}"
 echo -e "${CYAN} Instalando K3S en modo SERVER en mlezcanoS...${NC}"
 echo -e "${CYAN}=========================================================${NC}"
 
-# Explicación de las flags:
-# server                		: Inicia K3s en modo servidor (Control Plane).
-# --write-kubeconfig-mode 644	: Da permisos de lectura a la configuración para usar 'kubectl' sin 'sudo'.
+# Insatalación del nodo servidor (control plane) de K3s:
+# ... server                	: Inicia K3s en modo servidor (Control Plane).
+# --write-kubeconfig-mode 644	: Permisos de lectura a la configuración para usar 'kubectl' sin 'sudo'.
 # --tls-san=$SERVER_IP   		: Incluye la IP privada en el certificado SSL para evitar errores de conexión segura.
 # --node-ip=$SERVER_IP   		: Le indica al clúster la IP privada exacta de esta máquina.
 # --bind-address=$SERVER_IP 	: Fuerza a la API de Kubernetes a escuchar conexiones solo en la IP privada.
@@ -33,14 +31,14 @@ curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="server \
   --bind-address $SERVER_IP \
   --flannel-iface $IFACE" sh -
 
-# Espera a que K3s genere el token que necesitarán los workers para unirse al clúster.
-echo  -e "${CYAN}Esperando el token de nodo de K3s...${NC}"
+# Generamos el token de nodo de K3s para que los nodos worker puedan unirse al clúster.
+echo  -e "${CYAN}Generando token de nodo de K3s...${NC}"
 while [ ! -f /var/lib/rancher/k3s/server/node-token ]; do
   sleep 2
 done
 
-# Copia el token a /vagrant (directorio compartido con la máquina anfitriona/host).
-# De este modo, los nodos worker podrán leerlo al iniciar para unirse al clúster.
+# Copia el token a /vagrant (directorio compartido con la máquina anfitriona/host),
+# para que los nodos worker puedan leerlo al iniciar para unirse al clúster.
 cp /var/lib/rancher/k3s/server/node-token /vagrant/node-token
 
 echo  -e "${CYAN}Instalación del Server k3s Completada!${NC}"
--- p1/scripts/worker.sh
+++ p1/scripts/worker.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# scripts/worker.sh
-# Este script se ejecuta tras la instalación del servidor K3s. (server.sh)
 
-# script basado en p1/scripts/server.sh
-# para más explicaciones sobre las diferentes partes del script, 
-# es recomendable consultar el archivo p1/scripts/server.sh
+# Para más explicaciones es recomendable ver p1/scripts/server.sh primero
 
 CYAN='\033[0;36m'
-NC='\033[0m' # No Color
+NC='\033[0m'
 
 set -euo pipefail
 
-# Se pasan ambas IPs como argumentos desde el Vagrantfile.
 SERVER_IP=$1
 WORKER_IP=$2
 IFACE=$(ip -4 addr show | grep $WORKER_IP | awk '{print $NF}')
@@ -20,21 +15,20 @@ echo -e "${CYAN}=========================================================${NC}"
 echo -e "${CYAN} Instalando K3S en modo AGENT en mlezcanoSW...${NC}"
 echo -e "${CYAN}=========================================================${NC}"
 
-# Espera a que el server node genere el token de conexión en la carpeta compartida
-echo -e "${CYAN}Esperando por el node-token de K3s Server...${NC}"
+echo -e "${CYAN}Esperando por el node-token de K3s Server para establecer conexión...${NC}"
 while [ ! -f /vagrant/node-token ]; do
   sleep 2
 done
 
-# Leemos el token guardado por el servidor
+# leemos y almacenamos el token de K3s Server para que el nodo worker pueda unirse al clúster.
 TOKEN=$(cat /vagrant/node-token)
 
-# Explicación de las flags:
-# agent                			: Indica que el nodo ejecutará cargas de trabajo (Worker)
+# Intalación del nodo worker (agent) de K3s:
+# ... agent                		: Indica que el nodo ejecutará cargas de trabajo (Worker)
 # --server https://... 			: Dirección y puerto (6443) del servidor K3s al que se unirá.
-# --token $TOKEN       			: Clave de autenticación para unirse al clúster de forma segura.
+# --token $TOKEN       			: Token de autenticación para unirse al clúster de forma segura.
 # --node-ip $WORKER_IP 			: Asigna la IP privada exacta de este nodo worker.
-# --flannel-iface $IFACE		: Obliga a la red interna de Pods (Flannel) a usar la interfaz privada calculada.
+# --flannel-iface $IFACE		: Obligamos a la red interna de Pods (Flannel) usar interfaz privada calculada.
 
 curl -sfL https://get.k3s.io | INSTALL_K3S_EXEC="agent \
   --server https://$SERVER_IP:6443 \
